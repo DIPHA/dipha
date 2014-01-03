142645953fb42962a9f669ada6ba2bc916e723bb
jan.reininghaus <jan.reininghaus@gmail.com>
2014-01-03 16:34:49 +0000

    now using CTest framework

---
+++ tests/test_primal_extrinsic_full_rips.cpp
@@ -0,0 +1,44 @@
+/*  Copyright 2014 IST Austria
+
+Contributed by: Jan Reininghaus
+
+This file is part of DIPHA.
+
+DIPHA is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DIPHA is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with DIPHA.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <dipha/includes.h> 
+
+int main( int argc, char** argv )
+{
+    MPI_Init( &argc, &argv );
+
+    dipha::inputs::full_rips_complex complex;
+    complex.load_binary( "extrinsic_full_rips.complex" );
+    dipha::data_structures::distributed_vector< int64_t > filtration_to_cell_map;
+    dipha::data_structures::write_once_column_array reduced_columns;
+    dipha::algorithms::compute_reduced_columns( complex, false, filtration_to_cell_map, reduced_columns );
+    dipha::outputs::save_persistence_diagram( "DIPHA_TEST_extrinsic_full_rips.diagram", complex, filtration_to_cell_map, reduced_columns, false );
+    MPI_Barrier( MPI_COMM_WORLD );
+    bool result_has_not_changed = dipha::mpi_utils::are_files_equal( "DIPHA_TEST_extrinsic_full_rips.diagram", "extrinsic_full_rips.diagram" );
+    MPI_Barrier( MPI_COMM_WORLD );
+
+    if( dipha::mpi_utils::is_root( ) ) {
+        std::remove( "DIPHA_TEST_extrinsic_full_rips.diagram" );
+        if( !result_has_not_changed ) {
+            MPI_Abort( MPI_COMM_WORLD, EXIT_FAILURE );
+        }
+    }
+
+    MPI_Finalize();
+}
